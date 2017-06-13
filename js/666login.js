d0e02ac19ff5b7e0c55d7f763697fa48d13113f5
Daave <daave@hitler.rocks>
2017-06-13 21:54:35 +0100

    Added a "secret" login, edited css and made some error pages and a .htaccess

---
+++ js/666login.js
@@ -0,0 +1,13 @@
+
+Spry.Utils.addLoadListener(function() {
+
+var modal = document.getElementById('id01');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+});
